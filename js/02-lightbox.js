434d916749e3dd28049767bc266d85084eab5833
Luiza Raczyło <luiza.raczylo@gmail.com>
2022-10-09 11:56:07 +0200

    commit

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+
+const createGallery = galleryItems
+  .map(
+    (item) =>
+      `<a class="gallery__item" href="${item.original}">
+          <img
+            class="gallery__image"
+            src="${item.preview}" 
+            data-source="${item.original}"
+            alt="${item.description}"
+          />
+        </a>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", createGallery);
 
 console.log(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    canimationSpeed: 250,
+    captionPosition: 'bottom',
+});
+gallery.addEventListener("click",lightbox);
